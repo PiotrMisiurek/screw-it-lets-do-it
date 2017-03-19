d87f9b3e4e4c383d871213f5dedfc7bee6451757
Piotr Misiurek <p.misiurek@gmail.com>
2017-03-19 14:24:08 +0700

    Multi-sig transaction: transfer from multi-sig address (2of3)

---
+++ multi-sig/transfer_from_address.js
@@ -0,0 +1,48 @@
+var bitcore = require("bitcore-lib");
+
+var privateKeys = [
+  bitcore.PrivateKey.fromWIF("Kyu2q39avSmjuJbFp9mfh5mh8ZPYpyDxfZvRLSrcosdBiX6xWbG1"),
+  bitcore.PrivateKey.fromWIF("KwJ5KYx4fDCNEHG4jmmoQUveGkSxEJ4CqeEpTTDNgP5hmQLt41Ex")
+];
+
+var publicKeys = [
+  "02284cc8b2717d564d79cae638679ca1afd4a0f84fdc3ddb5fa1fe4698423d836b",
+  "03a98271626ff1bdff6dd18393b565e8675124ab43eab24efb6af352a06b56f91a",
+  "02d9e82923328f3dc434b8ff796c720321ce4e4874f248a2a8c38c1878717ca9f8"
+];
+
+var sourceAddress = "2NCv16HwcaTy2NV3XRSSfUEqmubhYesZfPm";
+
+var targetAddress = (new bitcore.PrivateKey).toAddress(bitcore.Networks.testnet);
+
+console.log("targetAddress: " + targetAddress);
+
+var Insight = require("bitcore-explorers").Insight;
+var insight = new Insight("testnet");
+
+insight.getUnspentUtxos(sourceAddress, function(error, utxos){
+  if (error) {
+    console.log(error);
+  } else {
+    var tx = new bitcore.Transaction();
+    tx.from(utxos, publicKeys, 2);
+    tx.to(targetAddress, 48792);
+    tx.change(sourceAddress);
+    tx.sign(privateKeys[0]);
+
+
+    serialized = tx.toObject();
+
+    var tx2 = new bitcore.Transaction(serialized);
+    tx2.sign(privateKeys[1]);
+
+
+    insight.broadcast(tx2.serialize(), function(error, transactionId) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(transactionId);
+      }
+    });
+  }
+});
